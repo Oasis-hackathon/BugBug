b269d9dc0c4231e9917a01a5d83339e6113f5f05
KangbeenKo <sweetrainforyou@gmail.com>
2021-01-02 17:58:01 +0900

    Plus Saramin Tutorial

--- Scrapper.py
+++ Scrapper.py
@@ -5,6 +5,9 @@ import pandas as pd
 indeed_LIMIT = 50
 indeed_URL = f"https://kr.indeed.com/%EC%B7%A8%EC%97%85?q=python&limit={indeed_LIMIT}"
 
+saramin_LIMIT = 100
+saramin_URL = f"http://www.saramin.co.kr/zf_user/search/recruit?searchType=search&searchword=%ED%8C%8C%EC%9D%B4%EC%8D%AC&recruitPageCount={saramin_LIMIT}"
+
 def extract_pages(URL):
     results = requests.get(URL)
     soup = BeautifulSoup(results.text, "html.parser")
@@ -29,6 +32,18 @@ def extract_indeed(html):
     job_id = html["data-jk"]
     return {'SITE':'INDEED','Job': title, 'Company': company, "Location": location, "Link": f"https://kr.indeed.com/viewjob?jk={job_id}"}
 
+def extract_saramin(html):
+    title = html.find("h2", {"class": "job_tit"}).find("a")["title"]
+    company = html.find("div", {"class": "area_corp"})
+    company_anchor = company.find("a")
+    if company_anchor is not None:
+        company = company_anchor.string
+    else:
+        company = company.string
+    company = company.strip()
+    location = html.find("div", {"class": "job_condition"}).find("a").string
+    job_id = html["value"]
+    return {'SITE':'SARAMIN','Job': title, 'Company': company, "Location": location, "Link": f"http://www.saramin.co.kr/zf_user/jobs/relay/view?isMypage=no&rec_idx={job_id}"}
 
 def indeed_jobs(last_page):
     jobs = pd.DataFrame()
@@ -42,7 +57,22 @@ def indeed_jobs(last_page):
             job = extract_indeed(result)
             df = pd.DataFrame.from_dict([job])
             jobs = jobs.append(df)
-            print(jobs)
+            #print(jobs)
+    return jobs
+
+def saramin_jobs(last_page):
+    jobs = pd.DataFrame()
+    print('Start Scrapping from "SARAMIN"')
+    for page in range(last_page):
+        print(f"Scrapping page {page+1}")
+        result = requests.get(f"{saramin_URL}&start={page*saramin_LIMIT}")
+        soup = BeautifulSoup(result.text, "html.parser")
+        results = soup.find_all("div", {"class": "item_recruit"})
+        for result in results:
+            job = extract_saramin(result)
+            df = pd.DataFrame.from_dict([job])
+            jobs = jobs.append(df)
+            #print(jobs)
     return jobs
 
 
@@ -50,9 +80,15 @@ def give_me_job(URL1, URL2):
 
     #INDEED
     last_page_1 = extract_pages(URL1)
+    print("INDEED Last Page:",last_page_1)
     indeed = indeed_jobs(last_page_1)
+
+    #SARAMIN
+    last_page_2 = extract_pages(URL2)
+    print("SARAMIN Last Page:",last_page_2)
+    saramin = extract_saramin_jobs(last_page_2)
 
     #return My_Job.to_excel('./Jobs(Python).xlsx')
 
 #Trial
-give_me_job(indeed_URL)
+give_me_job(indeed_URL, saramin_URL)
